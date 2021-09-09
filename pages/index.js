2d8e09f2f0fc445e2a08ee7a7a4dc0cc1f28ced0
DevMafee <salmansajibro@gmail.com>
2021-09-09 10:50:32 +0600

    Testing 2

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            Md Salman Sajib
           </span>
         </a>
       </footer>
