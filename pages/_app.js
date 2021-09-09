fc77d54ca24177f73d2bd355879d04f88b173cc4
DevMafee <salmansajibro@gmail.com>
2021-09-09 16:35:05 +0600

    Completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,36 @@
-import { StoreProvider } from '../components/Store'
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { StoreProvider } from '../components/Store';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 import Router from 'next/router';
+import { useEffect } from 'react';
 
-Router.events.on('routeChangeStart', ()=>NProgress.start());
-Router.events.on('routeChangeComplete', ()=>NProgress.done());
-Router.events.on('routeChangeError', ()=>NProgress.done());
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-      <StoreProvider>
-        <Component {...pageProps} />
-      </StoreProvider>
-    );
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
 
-MyApp.getInitialProps = async()=>{
+MyApp.getInitialProps = async () => {
   return {
     pageProps: {
-      commercePublicKey: process.env.COMMERCE_PUBLIC_KEY
-    }
-  }
-}
+      commercePublicKey: process.env.COMMERCE_PUBLIC_KEY,
+    },
+  };
+};
--- utils/commerce.js
+++ utils/commerce.js
@@ -1,21 +1,18 @@
-// Import the Commerce module
 import Commerce from '@chec/commerce.js';
 
-// Create a Commerce instance
 let commerce = null;
-function getCommerce(commercePublicKey){
-    if(commerce){
-        return commerce;
-    }else{
-        const publicKey = commercePublicKey || process.env.COMMERCE_PUBLIC_KEY;
-        const devEnvironment = process.env.NODE_ENV === 'development';
-        if(devEnvironment && !publicKey){
-            throw Error('Commerce Public Key is Not Defined');
-        }
 
-        commerce = new Commerce(publicKey, devEnvironment);
-        return commerce;
+function getCommerce(commercePublicKey) {
+  if (commerce) {
+    return commerce;
+  } else {
+    const publicKey = commercePublicKey || process.env.COMMERCE_PUBLIC_KEY;
+    const devEnvironment = process.env.NODE_ENV === 'development';
+    if (devEnvironment && !publicKey) {
+      throw Error('Commerce public API key not found.');
     }
+    commerce = new Commerce(publicKey, devEnvironment);
+    return commerce;
+  }
 }
-
 export default getCommerce;
